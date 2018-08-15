3c9516425780e8f52178739329f9e45072f21adf
Alejandro Aguilar <alex30697@gmail.com>
2018-08-15 00:09:11 +0000

    Version 1.1

--- Task_manager.c
+++ Task_manager.c
@@ -4,40 +4,28 @@
 #include <string.h>
 
 typedef struct task{
-	int data;
+	int data; //1: a, 2
 	char priority;
 	char autostart;
 	float *return_addr;
-	float *task_begin;
+	void (*task_begin)(void);
 	char state;
 	struct task* next;
 }task;
 
-typedef void (*callback)(task* data);
-typedef void (*apuntador_tarea);
+
 
 void taskA(void);
 void taskB(void);
 void taskC(void);
+typedef void (*callback)(task* data);
 
 void activateTask(task* n)
 {
-    if(n != NULL){
-        if(n->data == 1){
-        	taskA();
-        }
-        else if (n->data == 2)
-        {
-        	taskB();
-        }
-        else if (n->data == 3)
-        {
-        	taskC();
-        }
-    }
+	n->task_begin();
 }
 
-task* create(int data, task* next, float *task_begin){
+task* create(int data, task* next, void (*task_begin)(void)){
 	task* new_task = (task*)malloc(sizeof(task));
     if(new_task == NULL)
     {
@@ -63,7 +51,7 @@ task* create(int data, task* next, float *task_begin){
     return new_task;
 }*/
 
-task* prepend(int data, task* head, float *task_begin)
+task* prepend(int data, task* head, void (*task_begin)(void))
 {
     task* new_task = create(data,head, task_begin);
     head = new_task;	
@@ -123,26 +111,31 @@ int main(int argc, char const *argv[])
 	//apuntador_tarea ptA;
 	//void (*ptA)(void) = taskA;
 	//ptA = taskA;
-	apuntador_tarea ptA = taskA;
-	printf("%p\n", ptA);
-	apuntador_tarea ptB = taskB;
-	printf("%p\n", ptB);
-	apuntador_tarea ptC = taskC;
-	printf("%p\n", ptC);
-	printf("***********************************\n");
-	task_A = prepend(1, task_A, ptA);
-	task_B = prepend(2, task_A, ptB);
-	task_C = prepend(3, task_B, ptC);
+	void (*apuntador_tarea_A)(void) = taskA;
+	void (*apuntador_tarea_B)(void) = taskB;
+	void (*apuntador_tarea_C)(void) = taskC;
+	//printf("%p\n",apuntador_tarea_A);
+	//printf("***********************************\n");
+	task_A = prepend(1, task_A, apuntador_tarea_A);
+	printf("%p\n",*apuntador_tarea_A);
+	printf("%p\n",&apuntador_tarea_A);
+	apuntador_tarea_A();
+	task_B = prepend(2, task_A, apuntador_tarea_B);
+	task_C = prepend(3, task_B, apuntador_tarea_C);
 	head = task_C;
-	//execute(head, activate_task);
+	execute(head, activate_task);
 	printf("%p\n", task_A->task_begin);
-	printf("%p\n", task_B->task_begin);
-	printf("%p\n", task_C->task_begin);
-	printf("***********************************\n");
+	//task_A->task_begin();
+	//printf("%p\n", task_B->task_begin);
+	//printf("%p\n", task_C->task_begin);
+	//printf("***********************************\n");
 	//taskA();
 	/*a =8;
 	int *pa ;
 	pa = &a;
 	printf("%d",&pa);*/
+	//ptA;
+	
 	return 0;
+
 }
