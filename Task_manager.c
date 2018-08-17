a8e9c367c970ce6ff89507a9049cac3b16a186c0
Alejandro Aguilar <alex30697@gmail.com>
2018-08-17 22:14:48 +0000

    Version 1.3
    - La nueva lista con los autostart ya se ordena automaticamente.

--- Task_manager.c
+++ Task_manager.c
@@ -17,10 +17,16 @@ task* tarea_actual; // 0:A, 1:B, 2:C
 void taskA(void);
 void taskB(void);
 void taskC(void);
+void taskD(void);
+void taskE(void);
+void taskF(void);
 typedef void (*callback)(task* data);
 void (*apuntador_tarea_A)(void) = taskA;
 void (*apuntador_tarea_B)(void) = taskB;
 void (*apuntador_tarea_C)(void) = taskC;
+void (*apuntador_tarea_D)(void) = taskD;
+void (*apuntador_tarea_E)(void) = taskE;
+void (*apuntador_tarea_F)(void) = taskF;
 
 void activateTask(task* n)
 {
@@ -67,12 +73,50 @@ task* checkAutoStart(task* headT){
     while(cursorT != NULL)
     {
     	if(cursorT->autostart == 1){
-    		headA = prepend(cursorT->priority, cursorT->autostart, cursorT->task_begin, cursorT->state, headA);
+    		headA = prepend(cursorT->priority, cursorT->autostart, cursorT->task_begin, 1, headA);
     	}
     	cursorT = cursorT->next;
     }
 
     return headA;
+}
+
+task* insertion_sort(task* head)
+{
+    task *x, *y, *e;
+ 
+    x = head;
+    head = NULL;
+ 
+    while(x != NULL)
+    {
+        e = x;
+        x = x->next;
+        if (head != NULL)
+        {
+            if(head->priority > e->priority)
+            {
+                y = head;
+                while ((y->next != NULL) && (e->priority < y->next->priority))
+                {
+                    y = y->next;
+                }
+                e->next = y->next;
+                y->next = e;
+            }
+            else
+            {
+                e->next = head;
+                head = e ;
+            }
+        }
+        else
+        {
+            e->next = NULL;
+            head = e ;
+        }
+    }
+    return head;
 }
 
 void execute(task* head, callback f)
@@ -109,6 +153,18 @@ void taskC(void){
 	printf("Soy tarea C \n");
 }
 
+void taskD(void){
+	printf("Soy tarea D \n");
+}
+
+void taskE(void){
+	printf("Soy tarea E \n");
+}
+
+void taskF(void){
+	printf("Soy tarea F \n");
+}
+
 int main(int argc, char const *argv[])
 {
 	callback activate_task = activateTask;
@@ -117,13 +173,22 @@ int main(int argc, char const *argv[])
 	task* task_A = NULL;
 	task* task_B = NULL;
 	task* task_C = NULL;
-	task_A = prepend(1, 1, apuntador_tarea_A, 0, task_A);
-	task_B = prepend(2, 0, apuntador_tarea_B, 0, task_A);
-	task_C = prepend(3, 0, apuntador_tarea_C, 0, task_B);
-	head = task_C;
+	task* task_D = NULL;
+	task* task_E = NULL;
+	task* task_F = NULL;
+	task_A = prepend(1, 0, apuntador_tarea_A, 0, task_A);
+	task_B = prepend(3, 1, apuntador_tarea_B, 0, task_A);
+	task_C = prepend(2, 1, apuntador_tarea_C, 0, task_B);
+	task_D = prepend(4, 0, apuntador_tarea_D, 0, task_C);
+	// Nueva prueba
+	task_E = prepend(6, 1, apuntador_tarea_E, 0, task_D);
+	task_F = prepend(5, 1, apuntador_tarea_F, 0, task_E);
+	head = task_F;
 	//apuntador_tarea_A();
-	execute(checkAutoStart(head), activate_task);
+	printf("lista principal \n");
 	execute(head, activate_task);
+	printf("lista ordenada \n");
+	execute(insertion_sort(checkAutoStart(head)) , activate_task);
 	//printf("%p\n", task_A->task_begin);
 	//task_A->task_begin();	
 	return 0;
