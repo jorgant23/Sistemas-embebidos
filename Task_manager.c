331cb6395bcb5298f50eb8dde799a8192d1ca7d1
Alejandro Aguilar <alex30697@gmail.com>
2018-08-17 20:42:10 +0000

    Version 1.2
    - Implementamos un metodo para crear una segunda lista con las tareas autostart.

--- Task_manager.c
+++ Task_manager.c
@@ -4,90 +4,98 @@
 #include <string.h>
 
 typedef struct task{
-	int data; //1: a, 2
 	char priority;
 	char autostart;
 	float *return_addr;
 	void (*task_begin)(void);
 	char state;
-	struct task* next;
+	struct task* next; //nodo
 }task;
 
-
+task* tarea_actual; // 0:A, 1:B, 2:C
 
 void taskA(void);
 void taskB(void);
 void taskC(void);
 typedef void (*callback)(task* data);
+void (*apuntador_tarea_A)(void) = taskA;
+void (*apuntador_tarea_B)(void) = taskB;
+void (*apuntador_tarea_C)(void) = taskC;
 
 void activateTask(task* n)
 {
 	n->task_begin();
 }
 
-task* create(int data, task* next, void (*task_begin)(void)){
+task* terminateTask(){
+	tarea_actual->state= 0;
+	return tarea_actual;
+}
+
+//act
+
+void chainTask(task* n){
+
+}
+
+task* create(char priority, char autostart, void (*task_begin)(void), char state, task* next){
 	task* new_task = (task*)malloc(sizeof(task));
     if(new_task == NULL)
     {
         printf("Error creating a new task.\n");
         exit(0);
     }
-    new_task->data = data;
-    new_task->next = next;
+    new_task->priority = priority;
+    new_task->autostart = autostart;
     new_task->task_begin = task_begin;
-    return new_task;
-}
-
-/*task* create(int data, task* next){
-	task* new_task = (task*)malloc(sizeof(task));
-    if(new_task == NULL)
-    {
-        printf("Error creating a new task.\n");
-        exit(0);
-    }
-    new_task->data = data;
+    new_task->state = state;
     new_task->next = next;
-
     return new_task;
-}*/
+}
 
-task* prepend(int data, task* head, void (*task_begin)(void))
+task* prepend(char priority, char autostart, void (*task_begin)(void), char state, task* head)
 {
-    task* new_task = create(data,head, task_begin);
+    task* new_task = create(priority, autostart, task_begin, state, head);
     head = new_task;	
     return head;
 }
 
-/*task* prepend(int data, task* head)
-{
-    task* new_task = create(data,head);
-    head = new_task;	
-    return head;
-}*/
-
-void traverse(task* head, callback f)
-{
-    task* cursor = head;
-    while(cursor != NULL)
+task* checkAutoStart(task* headT){
+	task* cursorT = headT;
+    task* headA = NULL;
+    
+    while(cursorT != NULL)
     {
-    	f(cursor);
-    	//printf("HOla amigos putos %d\n", cursor->data);
-        cursor = cursor->next;
-        
+    	if(cursorT->autostart == 1){
+    		headA = prepend(cursorT->priority, cursorT->autostart, cursorT->task_begin, cursorT->state, headA);
+    	}
+    	cursorT = cursorT->next;
     }
+
+    return headA;
 }
 
 void execute(task* head, callback f)
 {
+    tarea_actual = head;
+    while(tarea_actual != NULL)
+    {
+    	f(tarea_actual);
+    	tarea_actual = tarea_actual->next;
+        
+    }
+}
+
+/*void execute(task* head, callback f)
+{
     task* cursor = head;
     while(cursor != NULL)
     {
     	f(cursor);
-    	//printf("HOla amigos putos %d\n", cursor->data);
-        cursor = cursor->next;
+    	cursor = cursor->next;
         
     }
-}
+}*/
 
 void taskA(void){
 	printf("Soy tarea A \n");
@@ -104,38 +112,19 @@ void taskC(void){
 int main(int argc, char const *argv[])
 {
 	callback activate_task = activateTask;
+	
 	task* head = NULL;
 	task* task_A = NULL;
 	task* task_B = NULL;
 	task* task_C = NULL;
-	//apuntador_tarea ptA;
-	//void (*ptA)(void) = taskA;
-	//ptA = taskA;
-	void (*apuntador_tarea_A)(void) = taskA;
-	void (*apuntador_tarea_B)(void) = taskB;
-	void (*apuntador_tarea_C)(void) = taskC;
-	//printf("%p\n",apuntador_tarea_A);
-	//printf("***********************************\n");
-	task_A = prepend(1, task_A, apuntador_tarea_A);
-	printf("%p\n",*apuntador_tarea_A);
-	printf("%p\n",&apuntador_tarea_A);
-	apuntador_tarea_A();
-	task_B = prepend(2, task_A, apuntador_tarea_B);
-	task_C = prepend(3, task_B, apuntador_tarea_C);
+	task_A = prepend(1, 1, apuntador_tarea_A, 0, task_A);
+	task_B = prepend(2, 0, apuntador_tarea_B, 0, task_A);
+	task_C = prepend(3, 0, apuntador_tarea_C, 0, task_B);
 	head = task_C;
+	//apuntador_tarea_A();
+	execute(checkAutoStart(head), activate_task);
 	execute(head, activate_task);
-	printf("%p\n", task_A->task_begin);
-	//task_A->task_begin();
-	//printf("%p\n", task_B->task_begin);
-	//printf("%p\n", task_C->task_begin);
-	//printf("***********************************\n");
-	//taskA();
-	/*a =8;
-	int *pa ;
-	pa = &a;
-	printf("%d",&pa);*/
-	//ptA;
-	
+	//printf("%p\n", task_A->task_begin);
+	//task_A->task_begin();	
 	return 0;
-
 }
